ff17557e3041ae14eabfd65870f2a8ad0c945869
Sadique Shafi <eglobenovuspark@gmail.com>
2026-06-04 15:57:17 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,4 @@ export const routes: Routes = [
         {path:'',loadComponent:()=>import('./features/home/pages/home/home.component').then((m)=> m.HomeComponent)},
         {path:'about',loadComponent:()=>import('./features/about/pages/about-page/about-page.component').then((m)=> m.AboutPageComponent)},
         {path:'test',loadComponent:()=>import('./features/tests/pages/test-list-page/test-list-page.component').then((m)=> m.TestListPageComponent)},
-
 ];

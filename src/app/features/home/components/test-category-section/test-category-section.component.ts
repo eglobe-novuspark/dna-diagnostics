4aa92f7fbaa0c2e21dda8d8406e09da3983d2c4c
Sadique Shafi <eglobenovuspark@gmail.com>
2026-06-06 00:38:32 +0530

    added contact blog whatup icon

--- src/app/features/home/components/test-category-section/test-category-section.component.ts
+++ src/app/features/home/components/test-category-section/test-category-section.component.ts
@@ -1,16 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { TESTS_MOCK } from '../../data/tests.mock';
 
 @Component({
   selector: 'app-test-category-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './test-category-section.component.html',
   styleUrl: './test-category-section.component.scss'
 })
 export class TestCategorySectionComponent {
-testCategoryItems = TESTS_MOCK.slice(0, 4).map((test, index) => ({
+
+  testCategoryItems = TESTS_MOCK.slice(0, 4).map((test, index) => ({
     ...test,
     displayCategory: this.getDisplayCategory(test.category),
     discount: [35, 44, 50, 50][index] || 40,
@@ -18,12 +23,24 @@ testCategoryItems = TESTS_MOCK.slice(0, 4).map((test, index) => ({
   }));
 
   private getDisplayCategory(cat: string): string {
+
     switch (cat.toLowerCase()) {
-      case 'dna-test': return 'DNA TEST';
-      case 'health-test': return 'DNA AGE TEST';
-      case 'nipt-test': return 'NIPT TEST';
-      case 'wellness-test': return 'ANCESTRY';
-      default: return 'DNA TEST';
+
+      case 'dna-test':
+        return 'DNA TEST';
+
+      case 'health-test':
+        return 'DNA AGE TEST';
+
+      case 'nipt-test':
+        return 'NIPT TEST';
+
+      case 'wellness-test':
+        return 'ANCESTRY';
+
+      default:
+        return 'DNA TEST';
     }
   }
+
 }

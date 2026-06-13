56d4e72bd3f0f8be80c775b50a499e1adad2daff
Sadique Shafi <eglobenovuspark@gmail.com>
2026-06-13 11:37:41 +0530

    testimonial update

--- src/app/features/home/components/home-testimonials/home-testimonials.component.ts
+++ src/app/features/home/components/home-testimonials/home-testimonials.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home-testimonials',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home-testimonials.component.html',
   styleUrl: './home-testimonials.component.scss'
 })
